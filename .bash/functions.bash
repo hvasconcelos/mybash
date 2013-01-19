cba5747ef7ced359b89a51739e0b24e390f4f85b
Hélder Vasconcelos <heldervasc@bearstouch.com>
2013-01-19 19:24:00 +0000

    idir functionality

--- .bash/functions.bash
+++ .bash/functions.bash
@@ -4,6 +4,7 @@ load_dir() {
   if [[ -d $1 ]]; then
     files=`find $1 -name *.bash`
     for file in $files; do
+      #echo "Loading file: $file"
       . $file
     done
   else
@@ -15,6 +16,36 @@ export_dir_after() {
   export PATH=$PATH:$*
 }
 
+addidir(){
+  echo "$1:$2" >> $HOME/.bash/IDIR
+}
+
+lidir(){
+  dirs=`cat $HOME/.bash/IDIR`
+  for entry in $dirs; do
+    name=`echo $entry | awk -F : '{print $1}'`
+    dir=`echo $entry | awk -F : '{print $2}'`
+    echo "$name $dir"
+  done
+}
+
+goto(){
+  if [[ $1 ]]; then
+    dirs=`cat $HOME/.bash/IDIR`
+    for entry in $dirs; do
+      name=`echo $entry | awk -F : '{print $1}'`
+      dir=`echo $entry | awk -F : '{print $2}'`
+      if [[ $name == $1 ]];then 
+        cd $dir
+        return 
+      fi
+    done
+    echo "Error: Directory entry not found"
+  else
+    echo "Syntax Error: goto <dir_entry>"
+  fi
+}
+
 export_dir_before(){
   export PATH=$*:$PATH
 }
@@ -22,12 +53,13 @@ export_dir_before(){
 set_profile(){
   if [[ -e $HOME/.bash/profiles/$1.bash ]]; then 
     echo $1 > $HOME/.bash/PROFILE
+    echo "Loading Profile [ $1 ]"
     . $HOME/.bash/profiles/$1.bash
   else
     echo "Error: Profile $1 doesn't exist" 
   fi
 }
-
+> $HOME/.bash/IDIR
 load_dir $HOME/.bash/plugin
 load_dir $HOME/.bash/alias
 
