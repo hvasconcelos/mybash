524850ea10c6bdbcdb4e8aebefe0cf865173a67a
Hélder Vasconcelos <heldervasc@bearstouch.com>
2013-01-29 21:25:41 +0000

    update fixes

--- installf.bash
+++ installf.bash
@@ -2,27 +2,26 @@
 # mybash install script
 # author: Helder Vasconcelos
 
-mybashu(){
-    cd $install_dir
-    git fetch >> $install_log_file 2>&1
-    remote_mcomm=$( cat .git/$(cat ~/.mybash/.git/refs/remotes/origin/HEAD | cut -d  ' ' -f 2 ) )
-    if [[ $( cat .git/refs/heads/master ) != $remote_mcomm ]]; then
-	mybashur $remote_mcomm
-    else
-      echo "mybash is up to date"
-    fi
-    cd $HOME
+mybash_update(){
+  cd $install_dir
+  git fetch >> $install_log_file 2>&1
+  remote_mcomm=$( cat .git/$(cat ~/.mybash/.git/refs/remotes/origin/HEAD | cut -d  ' ' -f 2 ) )
+  if [[ $( cat .git/refs/heads/master ) != $remote_mcomm ]]; then
+	   mybash_update_repo $remote_mcomm
+  else
+    echo "mybash is up to date"
+  fi
+  cd $HOME
 }
 
-mybashur(){
-    echo "Updating mybash to $1"
-    git stash >> $install_log_file 2>&1
-    if [[ $(git pull -f origin master >> $install_log_file 2>&1) -ne 0 ]]; then
-	echo "Error: Faileed to update mybash to version $1"
-	echo "Please verify the install log file $install_log_file"
-    else
-	echo "Succesfully Updated mybash to version $1"
-    fi
-    git stash pop >> $install_log_file 2>&1
-
+mybash_update_repo(){
+  echo "Updating mybash to $1"
+  git stash >> $install_log_file 2>&1
+  if [[ $(git pull -f origin master >> $install_log_file 2>&1) -ne 0 ]]; then
+	  echo "Error: Faileed to update mybash to version $1"
+	  echo "Please verify the install log file $install_log_file"
+  else
+    echo "Succesfully Updated mybash to version $1"
+  fi
+  git stash pop >> $install_log_file 2>&1
 }
