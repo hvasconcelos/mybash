4089179b52751e5f20012040ce4bf989b3f07bf2
Helder VAsconcelos <Helder.Vasconcelos@airtel-atn.com>
2013-01-30 10:49:37 +0000

    new alias: gitlg - git log graph added

--- alias/git.bash
+++ alias/git.bash
@@ -14,6 +14,7 @@ alias gitpl='git pull'
 alias gitrem='git remote -v'
 alias gitl='git log --color'
 alias gitl1='git log --oneline --color'
+alias gitlg="git log --graph --full-history --all --color"
 alias gits='git status'
 alias gitd='git diff --color'
 alias gitdone='git diff --oneline --color'
